55f04f914962ad4665f9f32e331abfd7805a138d
jhdh0813 <56108428+jhdh0813@users.noreply.github.com>
2020-09-29 14:26:07 +0900

    sage for test

---
+++ tutorial/hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("hello world!\n");
+
+    printf("i am C language!\n");
+
+    return 0;
+}
+
+/*
+    실행 방법: 터미널을 킨 후, g++ -o hello.c 를 입력한다. 그 후 ./a.exe 입력
+*/
---
+++ tutorial/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << endl;
+
+    cout << "i am C++ language!" << endl;
+
+    return 0;
+}
